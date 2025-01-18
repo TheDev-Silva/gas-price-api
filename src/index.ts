62cfa3f3c5280699e6538e641fa536fb43189282
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-01-18 11:48:31 -0300

    feat: estado anterior

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,21 @@ app.register(fuelPriceRoutes); // Registra as rotas
 app.register(userRoutes); // Registra as rotas
 app.register(gasStationRoutes); // Registra as rotas
 
-/* app.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
+app.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
   if (err) {
     console.log(err);
     process.exit(1);
   }
   console.log(`Server running at http://192.168.0.13:3000`);
-}); */
+});
 
 export default async (req: any, res: any) => {
-  await app.ready()
-  app.server.emit('request', req, res)
-  console.log('dados',res)
-}
+  try {
+    await app.ready();
+    app.server.emit('request', req, res);
+    console.log('Request handled');
+  } catch (err) {
+    console.error('Error handling request:', err);
+    res.send({ error: 'Internal Server Error' });
+  }
+};
