76b76cb172e6dfd8ee943bc20bdc6e046500f212
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-01-19 10:34:55 -0300

    feat: tentando reajustar

--- src/routes/gasStationRoutes.ts
+++ src/routes/gasStationRoutes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import { addGasStation, getGasStations } from '../controllers/gasStationController';
+import { authMiddleware } from '../utils/auth';
 
 const gasStationRoutes = async (fastify: FastifyInstance) => {
   fastify.post('/gas-station', addGasStation); // Add a new gas station
-  fastify.get('/gas-stations', getGasStations); // Get all gas stations
+  fastify.get('/gas-stations', { preHandler: authMiddleware }, getGasStations); // Get all gas stations
 };
 
 export default gasStationRoutes;
