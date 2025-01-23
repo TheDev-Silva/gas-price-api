5afc97d62f4c963664438c51ae60ac37609c8805
TheDev-Silva <silvadeveloper2024@gmail.com>
2025-01-23 06:49:44 -0300

    feat: removendo preHandle do gas-stations

--- src/routes/gasStationRoutes.ts
+++ src/routes/gasStationRoutes.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '../utils/auth';
 
 const gasStationRoutes = async (fastify: FastifyInstance) => {
   fastify.post('/gas-station', addGasStation); // Add a new gas station
-  fastify.get('/gas-stations', { preHandler: authMiddleware }, getGasStations); // Get all gas stations
+  fastify.get('/gas-stations', getGasStations); // Get all gas stations
 };
 
 export default gasStationRoutes;
